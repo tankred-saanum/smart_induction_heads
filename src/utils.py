3c291b3cd69a0d1261a14cc2bc80fac76a0b1bb7
Can Demircan <cdemircan@tutanota.com>
2025-09-02 11:32:51 +0200

    unique sequences

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,27 @@ def second_order_markov_sequence(tokens, args):
     all_tokens = torch.cat(all_tokens, dim=0)
     chunk_id=(torch.cdist(permuted_sequence.unsqueeze(-1).float(), permuted_sequence.unsqueeze(-1).float(), p=0) == 0).float().tril(diagonal=-1)
     return all_tokens, chunk_id
-    
+
+def unique_second_order_markov_sequence(tokens, args):
+    perms = []
+    used_perms_indices = set()
+    while len(perms) < args.n_permute:
+        perm_idx = torch.randperm(args.chunk_size)
+        perm_idx_tuple = tuple(perm_idx.tolist())
+        if perm_idx_tuple not in used_perms_indices:
+            used_perms_indices.add(perm_idx_tuple)
+            perms.append(tokens[perm_idx])
+        
+    ordered_sequence = torch.arange(args.n_reps*args.n_permute)%args.n_permute
+    permuted_sequence = ordered_sequence[torch.randperm(args.n_reps*args.n_permute)]
+    all_tokens = []
+    for seq_id in permuted_sequence:
+        all_tokens.append(perms[seq_id])
+
+    all_tokens = torch.cat(all_tokens, dim=0)
+    chunk_id=(torch.cdist(permuted_sequence.unsqueeze(-1).float(), permuted_sequence.unsqueeze(-1).float(), p=0) == 0).float().tril(diagonal=-1)
+    return all_tokens, chunk_id
+
     
 
 def third_order_markov_sequence(tokens, args):
@@ -39,6 +59,43 @@ def third_order_markov_sequence(tokens, args):
             _perm.append(perms[idx])
         _perm = torch.cat(_perm, dim=0)
         perms2.append(_perm)
+
+    ordered_sequence = torch.arange(args.n_reps*args.n_permute)%args.n_permute
+    permuted_sequence = ordered_sequence[torch.randperm(args.n_reps*args.n_permute)]
+    all_tokens = []
+    for seq_id in permuted_sequence:
+        all_tokens.append(perms2[seq_id])
+
+    all_tokens = torch.cat(all_tokens, dim=0)
+    chunk_id=(torch.cdist(permuted_sequence.unsqueeze(-1).float(), permuted_sequence.unsqueeze(-1).float(), p=0) == 0).float().tril(diagonal=-1)
+    return all_tokens, chunk_id
+
+
+def unique_third_order_markov_sequence(tokens, args):
+    
+    # first order perm
+    perms = []
+    used_perms_indices = set()
+    while len(perms) < args.n_permute_primitive:
+        perm_idx = torch.randperm(args.chunk_size)
+        perm_idx_tuple = tuple(perm_idx.tolist())
+        if perm_idx_tuple not in used_perms_indices:
+            used_perms_indices.add(perm_idx_tuple)
+            perms.append(tokens[perm_idx])
+        
+    # second order perm
+    perms2 = []
+    used_perms2_indices = set()
+    while len(perms2) < args.n_permute:
+        perm_idx = torch.randperm(args.n_permute_primitive)
+        perm_idx_tuple = tuple(perm_idx.tolist())
+        if perm_idx_tuple not in used_perms2_indices:
+            used_perms2_indices.add(perm_idx_tuple)
+            _perm = []
+            for idx in perm_idx:
+                _perm.append(perms[idx])
+            _perm = torch.cat(_perm, dim=0)
+            perms2.append(_perm)
 
     ordered_sequence = torch.arange(args.n_reps*args.n_permute)%args.n_permute
     permuted_sequence = ordered_sequence[torch.randperm(args.n_reps*args.n_permute)]
