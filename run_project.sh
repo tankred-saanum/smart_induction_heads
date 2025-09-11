eb7407b6d4d7c7b78422791e6329d8e005dadd5a
Can Demircan <cdemircan@tutanota.com>
2025-09-11 13:48:26 +0200

    hello 7b

--- run_project.sh
+++ run_project.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 python src/find_learning_heads.py --model_name Qwen/Qwen2.5-0.5B --markov_order=2 --batch_size=8
 python src/find_learning_heads.py --model_name Qwen/Qwen2.5-0.5B --markov_order=3 --batch_size=8
 
@@ -6,6 +7,9 @@ python src/find_learning_heads.py --model_name Qwen/Qwen2.5-1.5B --markov_order=
 
 python src/find_learning_heads.py --model_name Qwen/Qwen2.5-3B --markov_order=2 --batch_size=2
 python src/find_learning_heads.py --model_name Qwen/Qwen2.5-3B --markov_order=3 --batch_size=2
+
+python src/find_learning_heads.py --model_name Qwen/Qwen2.5-7B --markov_order=2 --batch_size=2
+python src/find_learning_heads.py --model_name Qwen/Qwen2.5-7B --markov_order=3 --batch_size=2
 
 
 python src/trace_nback2.py --model_name Qwen/Qwen2.5-0.5B --markov_order=2 --batch_size=8 --module=heads
